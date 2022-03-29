fe30bec2d3635447001d2291aaa13dd2ef8e5e68
Kyle Partridge <partkyle@gmail.com>
2022-03-28 19:49:28 -0700

    add tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ plugins {
 }
 
 val lwjglVersion = "3.0.0"
+val jupiterVersion = "5.8.1"
+val glmVersion = "1.0.1"
 
 dependencies {
     implementation("org.lwjgl:lwjgl:${lwjglVersion}")
@@ -17,5 +19,8 @@ dependencies {
     implementation("org.lwjgl:lwjgl-platform:${lwjglVersion}:natives-linux")
     implementation("org.lwjgl:lwjgl-platform:${lwjglVersion}:natives-osx")
 
-    implementation("io.github.java-graphics:glm:1.0.1")
+    implementation("io.github.java-graphics:glm:${glmVersion}")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${jupiterVersion}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${jupiterVersion}")
 }
